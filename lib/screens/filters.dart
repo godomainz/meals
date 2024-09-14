791b22f68221993530eac1d5f16670ee10a88537
Akila Mendis <godomainz@gmail.com>
2024-09-14 10:33:09 +0530

    190. Getting Started with Another Provider

--- lib/screens/filters.dart
+++ lib/screens/filters.dart
@@ -1,18 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:meals/providers/filters_provider.dart';
 import 'package:meals/widgets/filter_switch_tile.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-enum Filter { glutenFree, lactoseFree, vegetarian, vegan }
-
-class FiltersScreen extends StatefulWidget {
+class FiltersScreen extends ConsumerStatefulWidget {
   final Map<Filter, bool> currentFilters;
 
   const FiltersScreen({super.key, required this.currentFilters});
 
   @override
-  State<FiltersScreen> createState() => _FiltersScreenState();
+  ConsumerState<FiltersScreen> createState() => _FiltersScreenState();
 }
 
-class _FiltersScreenState extends State<FiltersScreen> {
+class _FiltersScreenState extends ConsumerState<FiltersScreen> {
   var _glutenFreeFilterSet = false;
   var _lactoseFreeFilterSet = false;
   var _vegitarianFreeFilterSet = false;
